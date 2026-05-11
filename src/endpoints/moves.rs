e9f4f57fb83449fa65aa98c4ca1931f3083d498b
simon0302010 <simon@hackclub.app>
2026-05-11 15:07:11 +0200

    Refactor folder structure

---
+++ src/endpoints/moves.rs
@@ -0,0 +1,46 @@
+use axum::{Json, extract::State, http::StatusCode};
+use serde::Serialize;
+
+use crate::{SharedBoard, pieces::SimplifiedMove};
+
+#[derive(Serialize)]
+pub struct GetMovesResponse {
+    success: bool,
+    text: String,
+    moves: Option<Vec<SimplifiedMove>>,
+    side_to_move: String
+}
+
+pub async fn get_legal_moves(
+    State(board): State<SharedBoard>
+) -> (StatusCode, Json<GetMovesResponse>) {
+    let board = board.lock().await;
+
+    let side_to_move = board.get_side_to_move().to_string();
+
+
+    let mut moves: Vec<SimplifiedMove> = Vec::new();
+    for board_move in board.get_legal_moves() {
+        let piece_move = match board_move.piece_move() {
+            Ok(mov) => mov,
+            Err(e) => return (StatusCode::INTERNAL_SERVER_ERROR, Json(GetMovesResponse {
+                success: false,
+                text: format!("Failed to get possible moves: {}", e),
+                moves: None,
+                side_to_move
+            }))
+        };
+
+        let source = piece_move.get_source_square().to_string().to_uppercase();
+        let destination = piece_move.get_destination_square().to_string().to_uppercase();
+
+        moves.push(SimplifiedMove { piece_type: piece_move.get_piece_type(), source, destination });
+    }
+
+    (StatusCode::OK, Json(GetMovesResponse {
+        success: true,
+        text: "Successfully fetched possible moves".to_string(),
+        moves: Some(moves),
+        side_to_move
+    }))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::sync::Arc;
 
-use axum::{Json, Router};
-use axum::extract::State;
-use axum::http::StatusCode;
+use axum::Router;
 use axum::routing::get;
 use libchess::ChessBoard;
-use serde::Serialize;
 use tokio::sync::Mutex;
 
 mod squares;
 mod pieces;
+mod endpoints;
 
-use crate::pieces::SimplifiedMove;
+use crate::endpoints::moves::get_legal_moves;
 
 type SharedBoard = Arc<Mutex<ChessBoard>>;
 
@@ -20,51 +18,9 @@ async fn main() {
     let board = Arc::new(Mutex::new(ChessBoard::default()));
 
     let app = Router::new()
-        .route("/moves", get(get_moves))
+        .route("/moves/get_legal", get(get_legal_moves))
         .with_state(board);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("Failed to setup listener");
     axum::serve(listener, app).await.expect("Failed to serve app");
-}
-
-#[derive(Serialize)]
-struct GetMovesResponse {
-    success: bool,
-    text: String,
-    moves: Option<Vec<SimplifiedMove>>,
-    side_to_move: String
-}
-
-async fn get_moves(
-    State(board): State<SharedBoard>
-) -> (StatusCode, Json<GetMovesResponse>) {
-    let board = board.lock().await;
-
-    let side_to_move = board.get_side_to_move().to_string();
-
-
-    let mut moves: Vec<SimplifiedMove> = Vec::new();
-    for board_move in board.get_legal_moves() {
-        let piece_move = match board_move.piece_move() {
-            Ok(mov) => mov,
-            Err(e) => return (StatusCode::INTERNAL_SERVER_ERROR, Json(GetMovesResponse {
-                success: false,
-                text: format!("Failed to get possible moves: {}", e),
-                moves: None,
-                side_to_move
-            }))
-        };
-
-        let source = piece_move.get_source_square().to_string().to_uppercase();
-        let destination = piece_move.get_destination_square().to_string().to_uppercase();
-
-        moves.push(SimplifiedMove { piece_type: piece_move.get_piece_type(), source, destination });
-    }
-
-    (StatusCode::OK, Json(GetMovesResponse {
-        success: true,
-        text: "Successfully fetched possible moves".to_string(),
-        moves: Some(moves),
-        side_to_move
-    }))
 }
